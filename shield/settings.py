f6873dfbf816853c1a684b8c6bc39773c2e47bdb
amnahhashmi <amnahashmi130@gmail.com>
2020-03-23 19:49:02 -0400

    removed secret key

--- shield/settings.py
+++ shield/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l*cq(9nrq5)2f0_mxg)e)7lwo_dvy$rvowt6y!y3p_yfqxk)%x'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
