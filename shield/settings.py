2b007e2eb9b09bf134137e0ed99e12a8a2b4503b
amnahhashmi <amnahashmi130@gmail.com>
2020-03-25 16:26:51 -0400

    static directory

--- shield/settings.py
+++ shield/settings.py
@@ -132,8 +132,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'errand_matcher/static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'errand_matcher')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
