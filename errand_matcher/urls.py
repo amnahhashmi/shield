705b29468733d615c0c71ac8199497203b039540
Suhas Vijaykumar <suhas.vk@gmail.com>
2020-03-24 21:06:59 -0400

    set up local env

--- errand_matcher/urls.py
+++ errand_matcher/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('requirements', views.requirements, name='requirements')
 ]
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/suhasvijaykumar/Dropbox (Personal)/Code/SHIELD/shield/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
