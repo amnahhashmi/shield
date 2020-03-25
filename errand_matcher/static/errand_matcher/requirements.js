6631e9cde3fbb147d757002a0ac949316c17bc69
Suhas Vijaykumar <suhas.vk@gmail.com>
2020-03-25 10:11:40 -0400

    added basic interaction to requirements page
    added basic interactivity

--- errand_matcher/static/errand_matcher/requirements.js
+++ errand_matcher/static/errand_matcher/requirements.js
@@ -1,3 +1,23 @@
 // requirements.js
 // script for 'requirements' endpoint
 // SV: TODO
+
+$(document).ready(function() {
+	
+	console.log('Page source loaded.')
+
+	$(".req-input").change(function(){
+		// If all requirements are checked
+    	if ($('.req-input:checked').length == $('.req-input').length) {
+    		// Show affirmative
+       		$('.affirmative').css('display','flex')
+       		// TODO: send to next view
+    	} 
+    	else {
+    		// Hide affirmative
+    		$('.affirmative').css('display','none')
+    	}
+
+	});
+
+});
