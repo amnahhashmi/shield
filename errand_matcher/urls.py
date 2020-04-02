929768a2075a773a2d27d3d7c5518dcdc064e246
sophiahaim <62728632+sophiahaim@users.noreply.github.com>
2020-04-02 15:08:32 -0700

    Merge urls.py properly

--- errand_matcher/urls.py
+++ errand_matcher/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('volunteer', views.begin_signup, name='begin_signup'),
     path('confirmation', views.confirm_email, name='confirm_email'),
     path('activate/<uuid:token_id>', views.activate, name='activate'),
-    path('signup', views.complete_signup, name='complete_signup'),
     path('health', views.health_and_safety, name='health_and_safety'),
 ]
