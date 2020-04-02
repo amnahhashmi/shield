2a0c6e548e102c33538e6cd2c38c54c9cfb4db2a
Suhas Vijaykumar <suhas.vk@gmail.com>
2020-04-01 23:05:03 -0400

    bones for address field

--- errand_matcher/static/errand_matcher/complete-signup.js
+++ errand_matcher/static/errand_matcher/complete-signup.js
@@ -1,6 +1,43 @@
 // complete-signup.js
 // script for 'begin_signup' endpoint
 
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle(
+          {center: geolocation, radius: position.coords.accuracy});
+      autocomplete.setBounds(circle.getBounds());
+    });
+  }
+}
+
+// var autocomplete;
+
+// // Initialize the Google Maps API autocomplete widget for address entry.
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search predictions to
+  // geographical location types.
+
+  autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('address-input'), {types: ['geocode']});
+
+  // Avoid paying for data that you don't need by restricting the set of
+  // place fields that are returned to just the address components.
+  autocomplete.setFields(['address_component','geometry']);
+
+  // When the user selects an address from the drop-down, populate the
+  // address fields in the form.
+  autocomplete.addListener('place_changed', function(){
+  	// Populate confirmation page and POST form
+  });
+}
+
 $(document).ready(function() {
 
 	var pages = $(".page")
@@ -23,9 +60,14 @@ $(document).ready(function() {
 		$("stop").slice(1,3).attr("offset",progressBarLocation)
 
 		// populate confirmation fields
-		$("#name-confirmation").text($("#firstname-input"+" "+$("#lastname-input")))
+		$("#name-confirmation").text($("#firstname-input").val()+" "+$("#lastname-input").val())
+
 		// todo: learnhow to retrieve email
-		$("#email-confirmation").text("")
+		$("#email-confirmation").text("NEED EMAIL FROM CONF-TOKEN")
+
+		$("#phone-confirmation").text($('#phone-input').val())
+
+		$("transport-confirmation").text($)
 
 		// todo handle button activation/deactivation
 	})
@@ -66,12 +108,15 @@ $(document).ready(function() {
 		if (phone.length==10) {
 			$('body').trigger("pageEvent", pageIndex + 1)
 		} else {
-			// display a warning message
+			// todo: display a warning message
 		}
 	})
 
+	$('#address-input').blur(function(e){
+		// todo: validate
+		$('body').trigger("pageEvent", pageIndex + 1)
+	})
 
-	
 	// display first page
 	$("body").trigger("pageEvent",0)
 
