b6bba4307767855078699cce1459d9b6faaa34fe
amnahhashmi <amnahashmi130@gmail.com>
2020-04-05 19:57:58 -0400

    does this work?

--- shield/settings.py
+++ shield/settings.py
@@ -24,15 +24,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'shieldcovid.herokuapp.com', 'www.livelyhood.io']
 
 CSRF_USE_SESSIONS = True
-CSRF_COOKIE_SECURE = True
-# SECURE_SSL_REDIRECT = False
-# SESSION_COOKIE_SECURE = True
-
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
