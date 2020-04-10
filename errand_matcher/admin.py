4e95fef2287abf9213de2b51ffcd6d92fb4ab53a
amnahhashmi <amnahashmi130@gmail.com>
2020-04-10 17:01:08 -0400

    export csv for admin

--- errand_matcher/admin.py
+++ errand_matcher/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
 from errand_matcher.models import User, Volunteer
+import csv
+from django.http import HttpResponse
 
-admin.site.register(User)
-admin.site.register(Volunteer)
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin, ExportCsvMixin):
+	actions = ["export_as_csv"]
+
+@admin.register(Volunteer)
+class VolunteerAdmin(admin.ModelAdmin, ExportCsvMixin):
+	actions = ["export_as_csv"]
+
