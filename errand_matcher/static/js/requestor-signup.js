d2ab9f41a9ad9fa9dbad3ba1d1e158f7fb71e33b
Suhas Vijaykumar <suhas.vk@gmail.com>
2020-04-10 00:15:09 -0400

    added interactions

--- errand_matcher/static/js/requestor-signup.js
+++ errand_matcher/static/js/requestor-signup.js
@@ -40,17 +40,16 @@ function initAutocomplete() {
 	autocomplete.addListener('place_changed', function(){
 
 		var place = autocomplete.getPlace();
-		var zip;
+		var address = {};
 
 		$.each(place ? place.address_components : [], function(){
-			if (this.types[0] == "postal_code") {
-				zip = this.long_name;
-			}
+			var property = {}
+			property[this.types[0]] = this.short_name;
+			$.extend(address, property)
 		});
 
-
 		// SV Question: What should we do if address is provided with no ZIP code (but valid lat/lng)?
-		if (!place || !place.geometry || !place.address_components || !zip) {
+		if (!place || !place.geometry || !address['postal_code']) {
 			// User entered the name of a Place that was not suggested and
 	    	// pressed the Enter key, or the Place Details request failed.
 			$("#address-warning").show();
@@ -58,10 +57,10 @@ function initAutocomplete() {
 		} else {
 			$("#address-input").attr("lat",place.geometry.location.lat())
 			$("#address-input").attr("lon",place.geometry.location.lng())
-			$("#address-input").attr("zip",zip)
-			$("#zipcode-review").text(zip)
-	
+
+			$("#address-review").text($("#address-input").val()+" "+address['postal_code'])
 			$('body').trigger('pageEvent', pageIndex + 1)
+
 		}
 	})
 }
@@ -90,63 +89,83 @@ $(document).ready(function() {
 
 	})
 
-	// button behavior
-	$(".next-button").click(function(){
+	// 
+	$("body").bind("partialSubmitEvent", function(e) {
 
-		if (pageIndex == 0) {
-			if ($('.req-input:checked').length == 0) {
-				$('#requirements-warning').show()
-			}
+		switch(pageIndex) {
+
+			// requirements checkbox validation
+			// requestor must meet at least one requirement
+			case 0:
+				if ($('.req-input:checked').length == 0) {
+					$('#requirements-warning').show().focus()
+					return;
+				} else {
+					break;
+				}
+
+			// name input validation
+			// requestor must enter something for both first and last name
+			case 1:
+				if ($('#firstname-input').val().length == 0 || $('#lastname-input').val().length == 0){
+					$('#name-warning').show().focus()
+					return;
+				} else {
+					$('#firstname-review').text($('#firstname-input').val());
+					$('#lastname-review').text($('#lastname-input').val());
+					break;
+				}
+
+			// date of birth validation
+			// must input a complete, valid date which is in the past
+			case 2:
+				date_input = moment($('#dob-input').val(),"YYYY-MM-DD")
+				if(!date_input.isValid() || !date_input.isBefore()){
+					// $("#dob-warning").show().focus()
+					return;
+					// TODO: further validation to accord with 65+ restriction? 
+				} else {
+					break;
+				}
+
+			case 4:
+				if ($("input[name='contact']:checked").length == 0) {
+					return;
+				} else {
+					$("#contact-review").text($("input[name='contact']:checked").val())
+					break;
+				}
+
+			case 5:
+				phone = $("#phone-input").val();
+				phone = phone.replace(/[^0-9]/g,'');
+
+				$("#phone-input").val(
+					(phone.slice(0,3) ? "("+phone.slice(0,3)+")" : "") +
+					(phone.slice(3,6) ? "-"+phone.slice(3,6) : "") +
+					(phone.slice(6,10) ? "-"+phone.slice(6,10) : "")
+				);
+
+				if (phone.length!=10) {
+					$("#phone-warning").show();
+					return;
+				} else {
+					// Set review field to user input
+					$('#phone-review').text($("#phone-input").val());
+					break;
+				}
 		}
-		
-		if (pageIndex == 1) {
-			if ($('#firstname-input').val().length > 0 && $('#lastname-input').val().length > 0){
-				// Set review field to user input
-				$('#firstname-review').text($('#firstname-input').val());
-				$('#lastname-review').text($('#lastname-input').val());
-				$('body').trigger("pageEvent", pageIndex + 1);
-			}
-			else
-				return;
-		}
-
-		// // Set transportation review to user input
-		// if (pageIndex == 3) {
-		// 	var transportations = $.map($("input[name='transport']:checked"), function(item){
-		// 		return $(item).val()
-		// 	})
-		// 	var transportation_as_str = transportations.join(", ")
-		// 	$('#transportation-review').text(transportation_as_str)
-		// }
-
-		// // Set frequencyreview to user input
-		// if (pageIndex == 4) {
-		// 	$('#frequency-review').text($("input[name='frequency']:checked").val())
-		// }
-
-		// // Set language review to user input
-		// if (pageIndex == 5) {
-		// 	var languages = $.map($("input[name='languages']:checked"), function(item){
-		// 		return $(item).val()
-		// 	})
-		// 	var languages_as_str = languages.join(", ")
-		// 	$('#language-review').text(languages_as_str)
-		// }
-
-		// // Validate user certified health and safety protocl
-		// if (pageIndex == 6) {
-		// 	if (!$("#certify-health-safety-protocol-cbox").is(":checked")) {
-		// 		$("#health-safety-warning").show()
-		// 		return;
-		// 	}
-		// }
 
 		$('body').trigger("pageEvent", pageIndex + 1)
 	})
 
+	// button behavior
+	$(".next-button").click(function(){
+		$('body').trigger("partialSubmitEvent")
+	})
+
 
 	$(".back-button").click(function(){
-		console.log(pageIndex)
 		if (pageIndex == 0) {
 			// send to landing page
 			window.location.pathname = '/';
@@ -155,34 +174,21 @@ $(document).ready(function() {
 		}
 	})
 
-	// If all name input fields are full on blur/defocus, automatically progress
-	$('#name-page').find(".text-input").blur(function(e){
-		if ($('#firstname-input').val().length > 0 && $('#lastname-input').val().length > 0){
+	$("#redo-button").click(function(){
+		window.location.pathname = "/request";
+	});
 
-			// Set review field to user input
-			$('#firstname-review').text($('#firstname-input').val());
-			$('#lastname-review').text($('#lastname-input').val());
-			$('body').trigger("pageEvent", pageIndex + 1);
+	// If all name input fields are full on blur/defocus, automatically progress
+	$('#firstname-input, #lastname-input').blur(function(e){
+		if ($('#firstname-input').val().length > 0 && $('#lastname-input').val().length > 0){
+			$('body').trigger("partialSubmitEvent");
 		}
 	});
 
-	// phone input cleaning + validation 
-	$('#phone-input').blur(function(e){
-		phone = $(this).val();
-		phone = phone.replace(/[^0-9]/g,'');
-
-		$(this).val(
-			(phone.slice(0,3) ? "("+phone.slice(0,3)+")" : "") +
-			(phone.slice(3,6) ? "-"+phone.slice(3,6) : "") +
-			(phone.slice(6,10) ? "-"+phone.slice(6,10) : "")
-		);
-
-		if (phone.length==10) {
-			// Set review field to user input
-			$('#phone-review').text(phone);
-			$('body').trigger("pageEvent", pageIndex + 1);
-		} else {
-			$("#phone-warning").show();
+	// single input blur with nonempty input -> progress form
+	$('#phone-input, #dob-input').blur(function(e){
+		if($(e.target).val().length > 0) {
+			$('body').trigger("partialSubmitEvent")
 		}
 	});
 
@@ -191,44 +197,32 @@ $(document).ready(function() {
 	});
 
 	// Submit volunteer form
-	// $(".finish-set-up").click(function(event){
-	// 	event.preventDefault();
-	// 	var csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-	// 	var add_volunteer_url;
-	// 	if (location.port) {
-	// 		 add_volunteer_url = window.location.protocol +'//' + document.domain + ':' + location.port + '/volunteer';
-	// 	} else {
-	// 		add_volunteer_url = window.location.protocol +'//' + document.domain + '/volunteer';
-	// 	}
+	$("#submit-button").click(function(event){
 
-	// 	// make POST ajax call
- //        $.ajax({
- //            type: 'POST',
- //            headers: {'X-CSRFToken': csrftoken},
- //            url: add_volunteer_url,
- //            data: {
- //            	"first_name": $("#firstname-review").text(),
- //            	"last_name": $("#lastname-review").text(),
- //            	"email": $("#email-review").text(),
- //            	"mobile_number": $("#phone-review").text(),
- //            	"frequency": $("#frequency-review").text(),
- //            	"transportation": $("#transportation-review").text(),
- //            	"language": $("#language-review").text(),
- //            	"lat": $("#address-input").attr("lat"),
- //            	"lon": $("#address-input").attr("lon")
- //            },
- //            success: function(response){
- //            	console.log(response);
- //            },
- //            error: function(jqHXR, exception){
- //            	console.log(exception);
- //            }
- //        })
- //        // set welcome field to user name
- //        $('#welcome-name').text('Welcome to the team, ' + $('#firstname-review').text());
- //        $('body').trigger("pageEvent", pageIndex + 1)
 
- //    })
+		// make POST ajax call
+        $.ajax({
+            type: 'POST',
+            headers: {'X-CSRFToken': csrftoken},
+            url: "/",
+            data: {
+            	"first_name": $("#firstname-review").text(),
+            	"last_name": $("#lastname-review").text(),
+            	"mobile_number": $("#phone-review").text(),
+            	"birth_date": $('#dob-input').val(),
+            	"contact_preference": $("#contact-review").text(),
+            	"lat": $("#address-input").attr("lat"),
+            	"lon": $("#address-input").attr("lon"),
+
+            },
+            success: function(response){
+            	console.log(response);
+            },
+            error: function(jqHXR, exception){
+            	console.log(exception);
+            }
+        })
+    })
 
 	// display first page
 	$("body").trigger("pageEvent",0)
