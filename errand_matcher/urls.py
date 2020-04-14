02fe73e50ba4c377acefdd055e98dd43774fb9b0
sophiahaim <62728632+sophiahaim@users.noreply.github.com>
2020-04-14 16:42:58 -0700

    Add basic UI for errand requests

--- errand_matcher/urls.py
+++ errand_matcher/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('confirmation', views.confirm_email, name='confirm_email'),
     path('activate/<uuid:token_id>', views.activate, name='activate'),
     path('health', views.health_and_safety, name='health_and_safety'),
-    path('requestor', views.requestor_signup, name='requestor')
+    path('requestor', views.requestor_signup, name='requestor'),
+    path('errand', views.request_errand, name='request_errand'),
 ]
