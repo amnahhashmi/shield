dd36deb064decd29639f8ff7d350b853adb0270c
amnahhashmi <amnahashmi130@gmail.com>
2020-05-11 13:05:42 -0400

    admin update

--- errand_matcher/admin.py
+++ errand_matcher/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from errand_matcher.models import User, Volunteer, Requestor
 from errand_matcher.models import ConfirmationToken
 from errand_matcher.models import Errand
+from errand_matcher.models import SiteConfiguration
 import csv
 from django.http import HttpResponse
 
@@ -25,17 +26,68 @@ class ExportCsvMixin:
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin, ExportCsvMixin):
-	actions = ["export_as_csv"]
+    actions = ["export_as_csv"]
 
 @admin.register(Volunteer)
 class VolunteerAdmin(admin.ModelAdmin, ExportCsvMixin):
-	actions = ["export_as_csv"]
+    list_display = ('get_user_first_name', 'get_user_last_name',
+        'get_user_email', 'mobile_number', 'lon', 'lat', 'frequency')
+    actions = ["export_as_csv"]
+
+    def get_user_first_name(self, obj):
+        return obj.user.first_name
+
+    def get_user_last_name(self, obj):
+        return obj.user.last_name
+
+    def get_user_email(self, obj):
+        return obj.user.email
+
+@admin.register(Requestor)
+class RequestorAdmin(admin.ModelAdmin, ExportCsvMixin):
+    actions = ["export_as_csv"]
+    list_display = ('get_user_name', 'mobile_number', 'lon', 'lat')
+
+    def get_user_name(self, obj):
+        return '{} {}'.format(obj.user.first_name,
+            obj.user.last_name)
+
+@admin.register(Errand)
+class ErrandAdmin(admin.ModelAdmin, ExportCsvMixin):
+    actions = ["export_as_csv"]
+    list_display = ('get_requestor_name', 'get_requestor_mobile_number', 
+        'requested_time', 'status', 'urgency', 'request_round', 'claimed_time', 
+        'get_claimed_volunteer_name', 'get_claimed_volunteer_mobile_number',
+        'completed_time')
+
+    def get_requestor_name(self, obj):
+        return '{} {}'.format(obj.requestor.user.first_name,
+            obj.requestor.user.last_name)
+
+    def get_requestor_last_name(self, obj):
+        return obj.requestor.user.last_name
+
+    def get_requestor_mobile_number(self, obj):
+        return obj.requestor.mobile_number
+
+    def get_claimed_volunteer_name(self, obj):
+        if obj.claimed_volunteer is not None:
+            return '{} {}'.format(obj.claimed_volunteer.user.first_name,
+                obj.claimed_volunteer.user.last_name)
+        else:
+            return None
+
+    def get_claimed_volunteer_mobile_number(self, obj):
+        if obj.claimed_volunteer is not None:
+            return obj.claimed_volunteer.mobile_number
+        else:
+            return None
+        
+
+admin.site.register(SiteConfiguration)
 
 @admin.register(ConfirmationToken)
 class ConfirmationTokenAdmin(admin.ModelAdmin, ExportCsvMixin):
     actions = ["export_as_csv"]
-
-admin.site.register(Requestor)
-admin.site.register(Errand)
 
 
