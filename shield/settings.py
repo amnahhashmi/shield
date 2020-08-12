bce77474faefe115f427122ffc39a955a6e7f7d0
amnahhashmi <amnahashmi130@gmail.com>
2020-08-12 15:51:37 -0400

    set debug to true

--- shield/settings.py
+++ shield/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 deploy_stage = os.environ.get('DEPLOY_STAGE')
 if deploy_stage in ['LOCAL', 'STAGING'] :
-    DEBUG = False
+    DEBUG = True
     SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = False
 else:
