74199406a004280b840fbe1f3c47ffca5a08341f
amnahhashmi <amnahashmi130@gmail.com>
2020-08-13 20:37:28 -0400

    wip

--- errand_matcher/static/js/requestor-signup.js
+++ errand_matcher/static/js/requestor-signup.js
@@ -25,7 +25,6 @@ function geolocate() {
 
 // // Initialize the Google Maps API autocomplete widget for address entry.
 function initAutocomplete() {
-
     // Create the autocomplete object, restricting the search predictions to
     // geographical location types.
     autocomplete = new google.maps.places.Autocomplete(
