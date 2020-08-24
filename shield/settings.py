8ccb273ae38700ef0fc2b89852a50899c27a326b
amnahhashmi <amnahashmi130@gmail.com>
2020-08-23 22:04:08 -0400

    rm staticfiles dirs

--- shield/settings.py
+++ shield/settings.py
@@ -156,11 +156,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'errand_matcher/static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Celery settings
